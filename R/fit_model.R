364cceceee02a42c98ec56d521a58d8779a889e0
Scott Claessens <scla896@UoA.auckland.ac.nz>
2024-10-17 13:26:56 +0100

    Prepare for model fitting

---
+++ R/fit_model.R
@@ -0,0 +1,18 @@
+# function to random intercept model
+fit_model <- function(data, measure = "Felicity") {
+  # subset data
+  data <- filter(data, Measure == measure)
+  # fit model
+  brm(
+    formula = Rating ~ 1 + (1 | PID) + (1 | Category/Item),
+    data = data,
+    family = cumulative,
+    prior = c(
+      prior(normal(0, 2), class = Intercept),
+      prior(exponential(3), class = sd)
+    ),
+    chains = 4,
+    cores = 4,
+    seed = 2113
+  )
+}
--- _targets.R
+++ _targets.R
@@ -1,5 +1,6 @@
 library(targets)
 library(tarchetypes)
+library(tidyverse)
 
 # set options for targets and source R functions
 tar_option_set(packages = c("brms", "tidyverse"))
@@ -7,5 +8,14 @@ tar_source()
 
 # targets pipeline
 list(
-  
+  # data file
+  tar_target(data_file, "data/pilot/pilot_data_clean.csv", format = "file"),
+  # load data
+  tar_target(data, read_csv(data_file, show_col_types = FALSE)),
+  # loop over two measures
+  tar_map(
+    values = tibble(measure = c("Felicity", "Sense")),
+    # fit model
+    tar_target(fit, fit_model(data, measure))
+  )
 )
